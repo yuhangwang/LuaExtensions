2bd22adcd154eda94aa59d61bb1fe06c948ba491
Yuhang Wang <stevenaura@outlook.com>
2015-06-18 14:06:20 -0500

    update file_module.lua

--- src/modules/file_module.lua
+++ src/modules/file_module.lua
@@ -37,4 +37,31 @@ function M.group_files_by_ext(list_file_names)
 	return dict_files_grouped
 end
 
+function M.concat(list_file_names, output_file_name)
+	-- Concatenate files
+	local OUT = io.open(output_file_name, "w")
+	io.output(OUT)
+	for i, file_name in pairs(list_file_names) do
+		local IN = io.open(file_name, 'r')
+		io.input(IN)
+		local txt_body = io.read("*a")
+		io.write(txt_body)
+		io.close(IN)
+	end
+	io.close(OUT)
+end
+
+function M.insert_id_column_before(file_input, file_output, scaling_factor)
+	local id_scaling_factor = scaling_factor or 1
+	local OUT = io.open(file_output, 'w')
+	io.output(OUT)
+	local ccc = 0 -- counter
+	for line in io.lines(file_input) do
+		local id = ccc * id_scaling_factor
+		io.write(string.format("%s %s\n", id, line))
+		ccc = ccc + 1
+	end
+	io.close(OUT)
+end
+
 return M
